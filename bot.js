fba88c53e82e3c525226d157d6ddf49b0ca93a36
StevenAbbott <sbabbott08@gmail.com>
2020-04-28 06:58:48 -0400

    All base functionality complete! Ready for launch!

--- bot.js
+++ bot.js
@@ -31,22 +31,24 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // so it will listen for messages that start with '!'
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
-        var cmd = args[0];
+        var cmd = args[0].toLowerCase();
 
         args = args.splice(1);
         switch (cmd) {
             // !ping
             case 'ping':
+                console.log("I got pinged by: " + user);
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
                 break;
-            case 'uCountingOnMe?':
-                console.log("Let's see if I'm tracking you for any words.");
+            case 'countingme?':
+                console.log("Let's see if I'm tracking " + user 
+                + " for any words.");
                 var words = getUsersWords(user);
                 var mes = "";
-                if (words != null) {
+                if (words != null && Object.keys(words).length > 0) {
                     mes += "Yes, supposedly you say some words a lot. Like:\n";
                     for (word in words) {
                         mes += " - " + word + " (" + words[word] + " times)\n";
@@ -60,6 +62,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 })
                 break;
             case botConfig.name:
+                console.log(user + " aka. " + toRealName(user) 
+                + " called in a request!");
                 console.log("target: " + args[0]);
                 console.log("word: " + args[2]);
                 var mes = "";
@@ -76,6 +80,31 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: mes
                 });
                 break;
+            case 'dontcountme':
+                console.log(user + " told me to never count them");
+                var mes = "";
+                if (Object.keys(data.people).includes(user)) {
+                    data.users[toRealName(user)] = {};
+                    mes = "Ok " + toRealName(user) 
+                    + " I will clear your record."
+                } else {
+                    mes = "I won't because I wasn't. I don't even know you."
+                }
+                saveData();
+                bot.sendMessage({
+                    to: channelID,
+                    message: mes
+                })
+                break;
+            case 'dontcountmeever':
+                console.log("user: " + user);
+                delete data.people[user];
+                saveData();
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Ok. I've forgotten you even exist."
+                })
+                break;
             case 'names':
                 console.log("Listed the names I know.");
                 var mes = "So far I know that:\n";
@@ -88,6 +117,26 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: mes
                 });
                 break;
+            case 'help':
+                console.log("Was asked for help. Tried my best *~~*");
+                bot.sendMessage({
+                    to: channelID,
+                    message: "My commands are:\
+                    \n - __'!help'__ - Congrats! You're already here.\
+                    \n- __'!ping'__ - see if I'm working\
+                    \n - __'!countingMe?'__ - See If I'm keeping track of how\
+ many times you use a certain word\
+                    \n - __'!sprocket (person's actual first name) says (word) \
+a lot'__ - I'll keep track of the given person's use of that\
+word *and let them know when they use it =P~~ ;)*\
+                    \n - __'!dontCountMe'__ - I'll forget all the words you're\
+being tracked for *pinky promise*\
+                    \n - __'!dontCountMeEver'__ I will forget your name *the \
+one steven hard-coded in* and never allow anyone to track your word usage \
+again!\
+                    \n - __'!names'__ - I'll give you a list of the names \
+I know."
+                });
             // Add other commands here...
         }
     }
